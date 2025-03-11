1a4e79d275610e6d214a603e8e27c97a8825a4fc
aguswesly <agustchannel@gmail.com>
2025-03-11 11:04:34 +0700

    feat : update testing

---
+++ test.ws
@@ -0,0 +1,4 @@
+let expr = 2 * (1+3);
+let cond = expr > 0 ? "Greater than 0" : "Smaller than 0";
+print(expr);
+print(cond);
--- ws_test.go
+++ ws_test.go
@@ -1,60 +1,29 @@
 package main
 
 import (
+	"os"
 	"testing"
 )
 
-func TestScanner(t *testing.T) {
-	input := "1 + -1 * 3"
-	expect := []*Token{
-		CreateToken(NUMBER, "1", "1", 1),
-		CreateToken(PLUS, "+", nil, 1),
-		CreateToken(MINUS, "-", nil, 1),
-		CreateToken(NUMBER, "1", "1", 1),
-		CreateToken(STAR, "*", nil, 1),
-		CreateToken(NUMBER, "3", "3", 1),
-		CreateToken(EOF, "EOF", nil, 1),
-	}
-
-	l := Lox{}
-	scanner := CreateScanner(input, &l)
-	tokens := scanner.scanTokens()
-
-	for i := 0; i < len(tokens); i += 1 {
-		if tokens[i].toString() != expect[i].toString() {
-			t.Errorf("Not match %s -> %s\n", tokens[i].toString(), expect[i].toString())
-		}
-	}
-}
-
 func TestScannerParserAndInterpreter(t *testing.T) {
-	input := "1 + -1 * 3"
-
-	right := CreateBinary(CreateUnary(CreateLiteral(1.0), CreateToken(MINUS, "-", nil, 1)), *CreateToken(STAR, "*", nil, 1), CreateLiteral(3.0))
-	expect := CreateBinary(CreateLiteral(1.0), *CreateToken(PLUS, "+", nil, 1), right)
-	expectedValue, err := expect.accept(&Interpreter{})
+	byt, err := os.ReadFile("test.ws")
 	if err != nil {
-		t.Error(err)
+		panic(err)
 	}
-
-	lox := Lox{}
-	scanner := CreateScanner(input, &lox)
-	tokens := scanner.scanTokens()
+	source := string(byt)
+	lox := Lox{
+		Interpreter: CreateInterpreter(),
+	}
+	scannner := CreateScanner(source, &lox)
+	tokens := scannner.scanTokens()
+	if lox.HadError {
+		t.Error("Error while scan")
+	}
 	parser := CreateParser(tokens, &lox)
-	inputTree, err := parser.parse()
+	statements, err := parser.parse()
+	lox.Interpreter.interpret(statements)
 	if err != nil {
 		t.Error(err)
-		return
-	}
-
-	inputValue, err := inputTree.accept(&Interpreter{})
-	if err != nil {
-		t.Error(err)
-		return
-	}
-
-	if inputValue != expectedValue {
-		t.Errorf("NOT EQUAL %s WITH %s\n", inputValue, expectedValue)
 		return
 	}
 }
